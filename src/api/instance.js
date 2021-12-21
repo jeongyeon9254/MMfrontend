2f87361484993958db373e0771405c76e15a5c69
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-12-21 10:57:35 +0900

    모듈별 instance 추가

---
+++ src/api/instance.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: '',
+
+  // 헤더에 넣을 정보
+  headers: {
+    Authorization: 토큰,
+    'content-type': 'application/json;charset=UTF-8',
+    accept: 'application/json',
+  },
+});
+
+//interceptors를 POST요청 가지고 결과 값을 캐치해서 return.
+instance.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+//interceptors를 GET요청 가지고 결과 값을 캐치해서 return.
+instance.interceptors.response.use(
+  response => {
+    const res = response.data;
+    return res;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
