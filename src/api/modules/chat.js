312f6b8b405c070bff69db2b5bf2baef43ad1c18
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-12-21 14:08:48 +0900

    instance 수정

--- src/api/modules/chat.js
+++ src/api/modules/chat.js
@@ -2,10 +2,8 @@ import instance from '../instance';
 
 // 승인 버튼
 export const getApproveDB = () => {
-  return function (dispatch, getState, { history }) {
-    instance({
-      method: 'get',
-      url: `/api/chemy/${userId}`,
-    });
-  };
+  instance({
+    method: 'get',
+    url: `/api/chemy/${userId}`,
+  });
 };
