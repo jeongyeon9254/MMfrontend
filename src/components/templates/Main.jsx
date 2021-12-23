461fd2c3ab1e1c31846f4ae5265f02337ce449e3
dogmaster <cwd3469@naver.com>
2021-12-23 09:53:16 +0900

    불필요한 요소 삭제

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import styled from 'styled-components';
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <div>
+      <TestWeb>
+        <TestWindow>
+          <button>사람</button>
+        </TestWindow>
+      </TestWeb>
+    </div>
+  );
 };
+
+const TestWeb = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #000;
+`;
+const TestWindow = styled.div`
+  width: 768px;
+  height: 1200px;
+  background-color: #eee;
+`;
 
 export default Main;
