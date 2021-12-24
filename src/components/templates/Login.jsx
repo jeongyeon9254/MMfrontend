d5121ed1bfdde0b72f852f31e373714c2c6a3df7
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-12-24 19:42:40 +0900

    [윤석준] 카카오 버튼  및 img  폴더 추가

--- src/components/templates/Login.jsx
+++ src/components/templates/Login.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import imgfile from '../../img/kakaologin.png';
 
 const Login = () => {
-  return <div>Login</div>;
+  const kakaoclick = () => {
+    window.location.href =
+      'https://kauth.kakao.com/oauth/authorize?client_id=5d14d9239c0dbefee951a1093845427f&redirect_uri=http://localhost:8080/user/kakao/callback&response_type=code';
+  };
+
+  return (
+    <>
+      <KaKaoBtn onClick={kakaoclick}></KaKaoBtn>
+    </>
+  );
 };
+
+const KaKaoBtn = styled.button`
+  background-image: url(${imgfile});
+  padding: 0;
+  width: 300px;
+  height: 45px;
+  line-height: 44px;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
 
 export default Login;
