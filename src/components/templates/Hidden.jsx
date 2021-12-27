3d417657bbed7ccd0fbb05430de35c179fae16e9
dogmaster <cwd3469@naver.com>
2021-12-27 19:46:13 +0900

    [ 최주영 ] hidden page

---
+++ src/components/templates/Hidden.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Input, Grid } from '../element';
+const Hidden = () => {
+  return (
+    <div>
+      <Grid border="1px solid #000" height="100vh" padding="30px"></Grid>
+    </div>
+  );
+};
+
+export default Hidden;
