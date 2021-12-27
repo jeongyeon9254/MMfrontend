851ccbf069198e0752dc43388616ae6f9a0c1768
jeongyeon9254 <jeongyeon9254@naver.com>
2021-12-28 00:26:10 +0900

    [정연재] map

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Grid from '../../components/element/Grid';
 import Header from '../../components/modules/layout/Header';
+import MapContainer from '../modules/main/MapContainer';
+import MapKategorieNav from '../modules/main/MapKategorieNav';
 
-const Main = () => {
+const Main = props => {
   return (
     <React.Fragment>
-      <Header>메인</Header>
-      <Grid padding="50px 0">ss</Grid>
+      <Header main>메인화면</Header>
+      <MapKategorieNav></MapKategorieNav>
+      <MapContainer></MapContainer>
     </React.Fragment>
   );
 };
