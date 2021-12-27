753f8e6b97a2b1b7a9ebe4c654b2dd4ce05acf75
jeongyeon9254 <jeongyeon9254@naver.com>
2021-12-27 15:51:01 +0900

    [정연재] gpsList 추가

---
+++ src/components/modules/map/gpsList.js
@@ -0,0 +1,127 @@
+const gpsLsit = [
+  {
+    location: '종로구',
+    latitude: 37.59708565258199,
+    longitude: 126.97061972318878,
+  },
+  {
+    location: '중구',
+    latitude: 37.560190897864906,
+    longitude: 126.99759845710656,
+  },
+  {
+    location: '용산구',
+    latitude: 37.53316613137628,
+    longitude: 126.98160092274547,
+  },
+  {
+    location: '성동구',
+    latitude: 37.55438802700418,
+    longitude: 127.04261109326114,
+  },
+  {
+    location: '광진구',
+    latitude: 37.54795065120619,
+    longitude: 127.08618628445537,
+  },
+  {
+    location: '동대문구',
+    latitude: 37.58201488151701,
+    longitude: 127.05536694729514,
+  },
+  {
+    location: '중랑구',
+    latitude: 37.59782740643261,
+    longitude: 127.0924422334882,
+  },
+  {
+    location: '성북구',
+    latitude: 37.60076412577266,
+    longitude: 127.02025770037797,
+  },
+  {
+    location: '강북구',
+    latitude: 37.64372246698335,
+    longitude: 127.01074149785245,
+  },
+  {
+    location: '도봉구',
+    latitude: 37.66935264903244,
+    longitude: 127.03313358882534,
+  },
+  {
+    location: '노원구',
+    latitude: 37.653239123910296,
+    longitude: 127.07542382059665,
+  },
+  {
+    location: '은평구',
+    latitude: 37.61913714094425,
+    longitude: 126.92646102493488,
+  },
+  {
+    location: '서대문구',
+    latitude: 37.57671367464311,
+    longitude: 126.93943995057974,
+  },
+  {
+    location: '마포구',
+    latitude: 37.560760023757126,
+    longitude: 126.90811246733483,
+  },
+  {
+    location: '양천구',
+    latitude: 37.521073264355316,
+    longitude: 126.85867787691797,
+  },
+  {
+    location: '강서구',
+    latitude: 37.56129666464895,
+    longitude: 126.82890328601613,
+  },
+  {
+    location: '구로구',
+    latitude: 37.496652116049916,
+    longitude: 126.85556388383493,
+  },
+  {
+    location: '금천구',
+    latitude: 37.46106931550623,
+    longitude: 126.90218472919369,
+  },
+  {
+    location: '영등포구',
+    latitude: 37.51826061974146,
+    longitude: 126.90983811339568,
+  },
+  {
+    location: '동작구',
+    latitude: 37.502349069229005,
+    longitude: 126.9540453463608,
+  },
+  {
+    location: '관악구',
+    latitude: 37.46857041258146,
+    longitude: 126.94599988124855,
+  },
+  {
+    location: '서초구',
+    latitude: 37.47094980878414,
+    longitude: 127.03231696067189,
+  },
+  {
+    location: '강남구',
+    latitude: 37.493950336362744,
+    longitude: 127.06261741977121,
+  },
+  {
+    location: '송파구',
+    latitude: 37.50401757964911,
+    longitude: 127.12045074883561,
+  },
+  {
+    location: '강동구',
+    latitude: 37.551317449147675,
+    longitude: 127.14797672715729,
+  },
+];
