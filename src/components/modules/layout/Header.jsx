c45a7e0451673dcdcfa29cfd33d78bd0ae9d30f1
dogmaster <cwd3469@naver.com>
2021-12-31 10:44:21 +0900

    [최주영] phone head reset

--- src/components/modules/layout/Header.jsx
+++ src/components/modules/layout/Header.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { Grid } from '../../element';
 import arrow_left from '../../../img/Icon/arrow_left.svg';
+import arrow_left_w from '../../../img/Icon/arrow_left_w.svg';
 import icon_search from '../../../img/Icon/icon_search.svg';
 import Phone_head from '../../../img/Icon/phone_head.png';
 // JS
 import { history } from '../../../redux/configureStore';
 
 const Header = props => {
-  const { children, main, chat, post, point, _on } = props;
+  const { children, main, chat, post, point, _on, bg, white } = props;
 
   const styles = {
     main,
     point,
+    bg,
+    white,
   };
 
   const goBack = () => {
@@ -32,10 +35,10 @@ const Header = props => {
               _on ? _on() : goBack();
             }}
           >
-            <img alt="뒤로가기 버튼" src={arrow_left}></img>
+            <img alt="뒤로가기 버튼" src={white ? arrow_left_w : arrow_left}></img>
           </div>
         )}
-        <TiTle>{children}</TiTle>
+        <TiTle {...styles}>{children}</TiTle>
         {chat ? <Search src={icon_search} /> : ''}
         {post ? <Exit>방 나가기</Exit> : chat ? '' : <Null></Null>}
       </Grid>
@@ -54,8 +57,8 @@ const HeaderStyle = styled.header`
   flex-direction: column;
   padding: 0px 23px 15px;
   border-radius: 30px 30px 0px 0px;
-  border-bottom: 1px solid #eee;
-  background-color: ${props => props.theme.colors.white};
+  border-bottom: ${props => (props.white ? '0px' : '1px solid #eee')};
+  background-color: ${props => (props.bg ? props.bg : props.theme.colors.white)};
   z-index: 99;
   gap: 13px;
   .backBtn {
@@ -67,7 +70,7 @@ const HeaderStyle = styled.header`
 `;
 const TiTle = styled.h2`
   font-size: ${props => props.theme.fontSizes.xxl};
-  color: ${props => props.theme.colors.gray_2};
+  color: ${props => (props.white ? '#fff' : props.theme.colors.gray_2)};
   position: absolute;
   left: 50%;
   top: 50%;
--- src/components/templates/Myinfo.jsx
+++ src/components/templates/Myinfo.jsx
@@ -4,16 +4,22 @@ import Header from '../modules/layout/Header';
 const Myinfo = () => {
   return (
     <div>
-      <Header>내 정보</Header>
+      <Header bg="transparent" white>
+        내 정보
+      </Header>
       <NewHeader></NewHeader>
     </div>
   );
 };
 
 const NewHeader = styled.div`
-  background-color: #000;
+  background-color: #3f3f41;
   width: 100%;
-  height: 50px;
+  height: 330px;
+  border-radius: 0px 0px 50px 50px;
+  position: fixed;
+  left: 0px;
+  top: 0px;
 `;
 
 export default Myinfo;
