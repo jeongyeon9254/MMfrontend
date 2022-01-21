63f146f845c809770282db0d30c2b07de346c3ce
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2022-01-21 13:56:35 +0900

    추가기입 수정

--- src/components/modules/Myinfo/MyinfoHead.jsx
+++ src/components/modules/Myinfo/MyinfoHead.jsx
@@ -27,7 +27,6 @@ function MyinfoHead(props) {
             </Grid>
             <Local>
               <Grid row gap="6px" align="center">
-
                 <img src={icon_location_w} alt="" />{' '}
                 {userInfo ? (
                   <span>
